4655b688a52e058de1e86e8af8a90d254f279149
Julia de Freitas Barbosa <139178072+lixzaura@users.noreply.github.com>
2026-05-12 19:48:04 -0300

    Menu bonitinho lindo e mágico

--- SoulMove.py
+++ SoulMove.py
@@ -22,9 +22,10 @@ creditos_acumulados = 0
 #missoes[i][0] é no nome da missao i. 
 #missoes[i][1] é os pontos da missao i.
 missoes = [
-    ["emita tantos carbonos", 30],
-    ["Ande de 6/7 bicicletas", 10],
-    ["Faça 6 viagens de tranporte publico em 7 dias", 67]
+    ["DIÁRIA: Faça um trajeto simples (curto) de bicicleta!", 50],
+    ["DIÁRIA: Faça um trajeto complexo (longo) utilizando o transporte público!", 100],
+    ["DIÁRIA: Assista 5 anúncios", 50],
+    ["SEMANAL: Use o transporte publico 5 dias seguidos", 350]
 ]
 
 #verificar se uma string é um float valido
@@ -92,86 +93,104 @@ def calcular_viagem(veiculo : str, km : float):
 
 
 def caso_ver_pontos():
-    print(f"Pontos: {pontos_acumulados}\nCreditos: {creditos_acumulados}")
+    print(f"⌞ .✦ ݁˖ Pontos: {pontos_acumulados} ⌝\n⌞ ⊹ ࣪ ˖ Creditos: R${creditos_acumulados} ⌝")
+    print("\n » Insira qualquer tecla para continuar.")
     input()
 
 def caso_comparar_emissao():
-    veiculo1 = input("Veiculo 1 (sem acentos): ")
-    
+    veiculo1 = input("⋮ » Veículo 1 (sem acentos): ")
+
     while(not veiculo_existe(veiculo1)):
-        print("Veiculo  não encontrado! Tente novamente sem acentos")
-        veiculo1 = input("Veiculo: ")
+        print("▸ Veículo não encontrado! Tente novamente sem acentos")
+        veiculo1 = input(" » Veículo: ")
 
-    veiculo2 = input("Veiculo 2 (sem acentos): ")
-    while(not veiculo_existe(veiculo2)):
-        print("Veiculo  não encontrado! Tente novamente sem acentos")
-        veiculo2 = input("Veiculo: ")
+    veiculo2 = input("⋮ » Veículo 2 (sem acentos): ")
 
+    while(not veiculo_existe(veiculo2) or veiculo1.lower().strip() == veiculo2.lower().strip()):
+
+        if veiculo1.lower().strip() == veiculo2.lower().strip():
+            print("▸ Os veículos não podem ser iguais! Digite outro veículo.")
+        else:
+            print("▸ Veículo não encontrado! Tente novamente sem acentos")
+
+        veiculo2 = input(" » Veículo 2: ")
 
     if obter_emissao(veiculo1) == 0 or obter_emissao(veiculo2) == 0:
 
-        print("Bicicleta não emite carbono")
+        print(".ᐟ ⌞ Bicicleta não emite carbono ⌝ .ᐟ")
 
         if veiculo1.lower().strip() == "bicicleta":
-            print(f"Emissao {veiculo2}: {obter_emissao(veiculo2)}")
+            print(f"⋮ » ⌞ Emissao {veiculo2}: {obter_emissao(veiculo2)} ⌝")
         else:
-            print(f"Emissao {veiculo1}: {obter_emissao(veiculo1)}")
+            print(f"Emissao {veiculo1}: {obter_emissao(veiculo1)} ⌝")
+
+        print("\n » Insira qualquer tecla para continuar.")
         input()
         return
 
     diff = abs(1 - comparar_emissao(veiculo1, veiculo2))
 
-    print(f"{veiculo1} emite {((diff * 100.00)):.2f}%", end=" ")
-    print("a menos") if menor_emissao(veiculo1, veiculo2) == veiculo1 else print("a mais")
+    print(f"⋮ » ⌞ {veiculo1} emite {((diff * 100.00)):.2f}%", end=" ")
+    print("a menos ⌝") if menor_emissao(veiculo1, veiculo2) == veiculo1 else print("a mais ⌝")
+
+    print("\n » Insira qualquer tecla para continuar.")
     input()
     os.system("cls")
 
 
 def caso_calcular_viagem():
-    veiculo = input("insira um veículo (sem acentos): ")
+    veiculo = input("⋮ » Insira um veículo (sem acentos): ")
     while not veiculo_existe(veiculo):
-        veiculo = input("Veiculo invalido!!\nInsira um veículo (sem acentos): ")
-    km = input("Quantos quilometros deseja calcular: ")
+        veiculo = input("▸ Veículo invalido!!\n⋮ » Insira um veículo (sem acentos): ")
+    km = input("⋮ » Quantos quilometros deseja calcular: ")
     while (not is_float(km)):
-        km = input("insira uma quilometragem válida: ")
+        km = input("⋮ » Insira uma quilometragem válida: ")
     emissao = obter_emissao(veiculo) * float(km)
 
-    print(f"{veiculo} emitirá {emissao:.1f}KG de carbono em {km}KM")
+    print(f"⋮ » ⌞ {veiculo} emitirá {emissao:.1f}KG de carbono em {km}KM ⌝")
+    
+    print("\n » Insira qualquer tecla para continuar.")
     input()
 
 
 def caso_verificar_emissao():
-    veiculo = input("insira um veículo (sem acentos): ")
+    veiculo = input("⋮ » Insira um veículo (sem acentos): ")
     while not veiculo_existe(veiculo):
-        veiculo = input("Veiculo invalido!!\nInsira um veículo (sem acentos): ")
+        veiculo = input("▸ Veiculo invalido!!\n⋮ » Insira um veículo (sem acentos): ")
     emissao = obter_emissao(veiculo)
-    print(f"A emissão de {veiculo} é de {emissao}KG por KM")
+    print(f"⋮ » ⌞ A emissão de {veiculo} é de {emissao}KG por KM ⌝")
+    
+    print("\n » Insira qualquer tecla para continuar.")
     input()
 
     
 
 
 def caso_ver_missao():
-    print("Missoes: ")
+    print(".✦ ݁˖ Missões: ")
 
     for i in range(len(missoes)):
         print()
-        print(f"Titulo: {missoes[i][0]}\nPontuacao: {missoes[i][1]}")
+        print(f"⌞ ⋮ » Titulo: {missoes[i][0]} ⌝ \n ⊹ ࣪ ˖ Pontuacao: {missoes[i][1]} ")
+
+    print("\n » Insira qualquer tecla para continuar.")
     input()
 
 
 def caso_completar_missao():
     global pontos_acumulados
-    print("Qual missao gostaria de realizou?")
+    print("⌞ Qual missao gostaria de registrar? ⌝")
     for i in range(len(missoes)):
-        print(f"{i+1} - {missoes[i][0]}. Pontos: {missoes[i][1]}")
-    escolha = int(input("Input: "))
+        print(f"\n⌞ ⋮ » {i+1} - {missoes[i][0]}. Pontos: {missoes[i][1]}⌝")
+    escolha = int(input(".ᐟ.ᐟ ─── Escolha: "))
     if escolha >= 1 and escolha <= len(missoes):
 
         pontos_acumulados += missoes[escolha-1][1]
-        print(f"Missao Concluida.\nAgora voce tem {pontos_acumulados} pontos")
+        print(f"\n⋮ » ⌞ Missao Concluida.\nAgora voce tem {pontos_acumulados} pontos ⌝")
     else:
-        print("Missao invalida")
+        print("▸ Missão invalida")
+
+    print("\n » Insira qualquer tecla para continuar.")
     input()
 
 
@@ -180,58 +199,76 @@ def caso_completar_missao():
 def caso_converter_pontos():
     global pontos_acumulados
     global creditos_acumulados
-    pontos = input(f"Voce tem {pontos_acumulados} pontos. Quantos pontos gostaria de converter: ")
+    pontos = input(f"⋮ » Voce tem {pontos_acumulados} pontos. Quantos pontos gostaria de converter: ")
     while(not pontos.isnumeric and pontos > pontos_acumulados):
-        print(f"insira um valor inteiro menor que {pontos_acumulados}: ")
-        pontos = input("quantos pontos gostaria de converter: ")
+        print(f"▸ Insira um valor inteiro menor que {pontos_acumulados}: ")
+        pontos = input("⋮ » Quantos pontos gostaria de converter: ")
     pontos_acumulados -= int(pontos)
     creditos_acumulados += float(pontos) * CREDITOS_POR_PONTO
-    print(f"isso equivale a {float(pontos) * CREDITOS_POR_PONTO} creditos\nSeu total agora é {creditos_acumulados}")
+    print(f"▸ Isso equivale a {float(pontos) * CREDITOS_POR_PONTO:.2f} creditos\n ⟢ ⌞ Você tem R${creditos_acumulados:.2f} ⌝ de créditos.")
+
+    print("\n » Insira qualquer tecla para continuar.")
     input()
     
     
 
 
 os.system("cls")
-nome = input("Nome: ")
+nome = input(".ᐟ.ᐟ ─── Insira seu nome: ")
 os.system("cls")
 
 escolha = -1
 while escolha != 0:
-    print(f"Ola {nome}, o que deseja fazer?")
+    print(f"⌞ Ola {nome}, o que deseja fazer? ⌝")
+    print("\n")
+    print("\t" + "⋮ ┆ 1 - Ver pontos e créditos")
+    print("\t" + "⋮ ┆ 2 - Comparar a emissão de dois veiculos")
+    print("\t" + "⋮ ┆ 3 - Calcular a emissão de uma viagem")
+    print("\t" + "⋮ ┆ 4 - Verificar a emissão de um veiculo")
+    print("\t" + "⋮ ┆ 5 - Ver missões disponíveis")
+    print("\t" + "⋮ ┆ 6 - Completar uma missão")
+    print("\t" + "⋮ ┆ 7 - Converter pontos em crédito")
+    print("\t" + "⋮ ┆ 0 - Sair do programa")
+    print("\n")
 
-    print("1 - Ver pontos e creditos")
-    print("2 - comparar a emissao de dois veiculos")
-    print("3 - Calcular a emissao de uma viagem")
-    print("4 - Verificar a emissao de um veiculo")
-    print("5 - Ver missoes")
-    print("6 - Completar missoes")
-    print("7 - Converter pontos em crédito")
-
-    print("\t0 - para sair")
-    escolha = input("Input: ")
+    escolha = input(".ᐟ.ᐟ ─── Insira a opção: ")
 
     os.system("cls")
 
     match escolha:
-        case '1': caso_ver_pontos()
+        case '1': 
+            caso_ver_pontos()
+            print("\n")
 
-        case '2': caso_comparar_emissao()
+        case '2': 
+            caso_comparar_emissao()
+            print("\n")
 
-        case '3': caso_calcular_viagem()
+        case '3': 
+            caso_calcular_viagem()
+            print("\n")
 
-        case '4': caso_verificar_emissao()
+        case '4': 
+            caso_verificar_emissao()
+            print("\n")
 
-        case '5': caso_ver_missao()
+        case '5': 
+            caso_ver_missao()
+            print("\n")
 
-        case '6': caso_completar_missao()
+        case '6': 
+            caso_completar_missao()
+            print("\n")
 
-        case '7': caso_converter_pontos()
+        case '7': 
+            caso_converter_pontos()
+            print("\n")
 
         case '0': break
 
         case _: 
-            print("Opcao invalida")
+            print("▸ Opção invalida!")
+            print("\n")
             #input()
 
 
