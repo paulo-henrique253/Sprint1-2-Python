8fa3b2dfcc52c3e8130106dc1bb20ee62b5fc4f8
Paulo Henrique <paulo253hgg@gmail.com>
2026-05-12 16:33:00 -0300

    Implementações implementadas

--- SoulMove.py
+++ SoulMove.py
@@ -7,6 +7,22 @@ EMISSAO_METRO = 0.005
 EMISSAO_MOTO = 0.08
 EMISSAO_ONIBUS = 0.04 #1.30kg que o onobus emite dividio pela média de passageiros (30 pessoas)
 EMISSAO_TREM = 0.005
+CREDITOS_POR_PONTO = 0.009
+
+
+pontos_acumulados = 0
+creditos_acumulados = 0
+
+
+#missoes[i][0] é no nome da missao i. 
+#missoes[i][1] é os pontos da missao i.
+missoes = [
+    ["emita tantos carbonos", 30],
+    ["Ande de 6/7 bicicletas", 10],
+    ["Faça 6 viagens de tranporte publico em 7 dias", 67]
+]
+
+
 
 
 total_carbono_emitido = 0
@@ -52,14 +68,18 @@ def calcular_viagem(veiculo : str, km : float):
     return emissao * km
 
 
+def caso_ver_pontos():
+    print(f"Pontos: {pontos_acumulados}\nCreditos: {creditos_acumulados}")
+    input()
+
 def caso_comparar_emissao():
-    veiculo1 = input("(sem acentos)Veiculo 1: ")
+    veiculo1 = input("Veiculo 1 (sem acentos): ")
     
     while(not veiculo_existe(veiculo1)):
         print("Veiculo  não encontrado! Tente novamente sem acentos")
         veiculo1 = input("Veiculo: ")
 
-    veiculo2 = input("(sem acentos)Veiculo 2: ")
+    veiculo2 = input("Veiculo 2 (sem acentos): ")
     while(not veiculo_existe(veiculo2)):
         print("Veiculo  não encontrado! Tente novamente sem acentos")
         veiculo2 = input("Veiculo: ")
@@ -84,6 +104,55 @@ def caso_comparar_emissao():
     os.system("cls")
 
 
+def caso_calcular_viagem():
+    veiculo = input("insira um veículo (sem acentos): ")
+    while not veiculo_existe(veiculo):
+        veiculo = input("Veiculo invalido!!\nInsira um veículo (sem acentos): ")
+    km = float(input("Quantos quilometros deseja calcular: "))
+    emissao = obter_emissao(veiculo) * km
+
+    print(f"{veiculo} emitirá {emissao:.1f}KG de carbono em {km}KM")
+    input()
+
+
+def caso_verificar_emissao():
+    veiculo = input("insira um veículo (sem acentos): ")
+    while not veiculo_existe(veiculo):
+        veiculo = input("Veiculo invalido!!\nInsira um veículo (sem acentos): ")
+    emissao = obter_emissao(veiculo)
+    print(f"A emissão de {veiculo} é de {emissao}KG por KM")
+    input()
+
+    
+
+
+def caso_ver_missao():
+    print("Missoes: ")
+
+    for i in range(len(missoes)):
+        print()
+        print(f"Titulo: {missoes[i][0]}\nPontuacao: {missoes[i][1]}")
+    input()
+
+
+def caso_completar_missao():
+    print("Qual missao gostaria de realizou?")
+    for i in range(len(missoes)):
+        print(f"{i+1} - {missoes[i][0]}. Pontos: {missoes[0][1]}")
+    escolha = input("Input: ")
+    if escolha >= 1 and escolha <= len(missoes)-1:
+        pass
+
+
+
+
+def caso_converter_pontos():
+    pontos = input(f"Voce tem {pontos_acumulados} pontos. Quantos pontos gostaria de converter: ")
+    while(not pontos.isnumeric and pontos > pontos_acumulados):
+        print(f"insira um valor inteiro menor que {pontos_acumulados}: ")
+        pontos = input("quantos pontos gostaria de converter: ")
+
+
 os.system("cls")
 nome = input("Nome: ")
 os.system("cls")
@@ -91,14 +160,38 @@ os.system("cls")
 escolha = -1
 while not escolha == 0:
     print(f"Ola {nome}, o que deseja fazer?")
-    print("1- comparar a emissao de dois veiculos")
-    print("2 - Calcular a emissao de 2 viagens")
-    print("3 - Verificar a emissao de um veiculo")
+
+    print("1 - Ver pontos e creditos")
+    print("2 - comparar a emissao de dois veiculos")
+    print("3 - Calcular a emissao de uma viagem")
+    print("4 - Verificar a emissao de um veiculo")
+    print("5 - Ver missoes")
+    print("6 - Completar missoes")
+    print("7 - Converter pontos em crédito")
+
     print("\t0 - para sair")
-    escolha = int(input())
+    escolha = input("Input: ")
+
     os.system("cls")
+
     match escolha:
-        case 1: caso_comparar_emissao()
+        case '1': caso_ver_pontos()
+
+        case '2': caso_comparar_emissao()
+
+        case '3': caso_calcular_viagem()
+
+        case '4': caso_verificar_emissao()
+
+        case '5': caso_ver_missao()
+
+        case '7': caso_converter_pontos()
+
+        case '0': break
+
+        case _: 
+            print("Opcao invalida")
+            #input()
 
 
 
